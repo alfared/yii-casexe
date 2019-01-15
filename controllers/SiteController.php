0a6741556e6686dffb951cad53d57ca5527489e5
Vitaliy Golovko <alfared3006@gmail.com>
2019-01-15 09:59:35 +0100

    Создание комманды для восстановление пароля

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -80,19 +80,19 @@ class SiteController extends Controller
      */
     public function actionLogin()
     {
-//        if (!Yii::$app->user->isGuest) {
-//            return $this->goHome();
-//        }
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
 //
-//        $model = new LoginForm();
+        $model = new LoginForm();
 //        if ($model->load(Yii::$app->request->post()) && $model->login()) {
 //            return $this->goBack();
 //        }
 //
 //        $model->password = '';
-//        return $this->render('login', [
-//            'model' => $model,
-//        ]);
+        return $this->render('login', [
+            'model' => $model,
+        ]);
 
     }
 
@@ -173,7 +173,19 @@ class SiteController extends Controller
 	 */
 	public function actionResetPassword($token)
 	{
+		try {
+			$model = new ResetPasswordForm($token);
+		}catch (InvalidaParamException $e) {
+			throw new BadRequestHttpException($e->getMessage());
+		}
 
+		if ($model->load(Yii::$app->request->post())
+		    && $model->validate()
+		    && $model->resetPassword()) {
+			return $this->goHome();
+		}
+
+		return $this->render('resetPassword', ['model' => $model]);
 	}
 
 
