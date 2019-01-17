52a7aa86b89ca648f336320ee7d8385ae3f1ae7a
Vitaliy Golovko <alfared3006@gmail.com>
2019-01-17 08:11:01 +0100

    Добавление шаблона для отправки формы на адрес

---
+++ controllers/CardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\models\Card;
+use app\models\Prize;
+
+class CardController extends Controller
+{
+    public function actionIndex() {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect('/site/login');
+        }
+
+        $payments = Card::find()->orderBy(['id' => SORT_DESC])->all();
+        return $this->render('payments', ['payments' => $payments]);
+    }
+
+	public function actionShow($id) {
+
+		$model = new Card();
+		$prize = Prize::find()->where(['id' => $id])->one();
+
+		$model->cardType = 1;
+		$model->amount = rand(0,100);
+
+		$this->view->title = 'Взять деньги на карту';
+		return $this->render('cardForm', compact('model'));
+
+	}
+
+}
---
+++ views/game/bonus.php
@@ -0,0 +1,4 @@
+<div class="prize-wrapper">
+	<p>Вы учли свой бонус</p>
+	<!--	<p><a id="playButton" class="btn btn-success prize-action" href="/game/prize/">PLAY!</a></p>-->
+</div>
